c9d1a32c50ba9998ccb14dda358ee0336b0ca56a
DESKTOP-351H9P8\Jeremy <jeremy.real@e-artsup.net>
2017-11-14 18:57:54 +0100

    Code Life

---
+++ Assets/Script/Life.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Life : MonoBehaviour
+{
+
+    public int FullHealth = 3;
+    public int CurHealth = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (CurHealth >= FullHealth)
+        {
+            CurHealth = FullHealth;
+        }
+
+        if (CurHealth <= 0)
+        {
+            CurHealth = 0;
+            Debug.Log("You are Dead");
+
+        }
+    }
+
+
+    void OntriggerEnter(Collider other)
+    {
+        (collision.gameObject.tag = "Obstacles");
+        CurHealth = -1;
+        Debug.Log("Hit");
+    }
+
+
+}
+
+
+
+     
+                
+
+
