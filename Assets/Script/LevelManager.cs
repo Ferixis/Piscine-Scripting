ee4b1a632007b3bfed0916e45d2ac9a35498e5e6
DESKTOP-859FG5E\jeremy <jeremy.real@e-artsup.net>
2017-11-15 10:28:45 +0100

    Life ok

---
+++ Assets/Script/LevelManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+    
+
+public class LevelManager : MonoBehaviour {
+
+    public int FullHealth = 3;
+    private int CurHealth ;
+
+    public static LevelManager Instance { get; private set; }
+
+    public TimeSpan RunningTime
+    {
+        get { return DateTime.UtcNow - _startedTime; }
+
+    }
+
+    private DateTime _startedTime;
+
+    void Awake()
+    {
+
+        Instance = this;
+        CurHealth = FullHealth;
+
+    }
+	// Use this for initialization
+	void Start () {
+
+        _startedTime = DateTime.UtcNow;
+	}
+	
+ 
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (CurHealth >= FullHealth)
+        {
+            CurHealth = FullHealth;
+        }
+
+        if (CurHealth <= 0)
+        {
+            CurHealth = 0;
+            Debug.Log("You are Dead");
+
+        }
+    }
+
+
+    public void TakeDmg(int DamageTaken)
+    {
+
+        CurHealth -= DamageTaken;
+        Debug.Log("HIT");
+    }
+
+
+}
---
+++ Assets/Script/Obstacles.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour {
+
+    public int Degats = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        PlayerMoves player = other.gameObject.GetComponentInParent<PlayerMoves>();
+        Debug.Log(player);
+
+        if(player)
+        {
+
+            LevelManager.Instance.TakeDmg(Degats);
+
+        }
+
+       
+           
+    }
+
+    }
